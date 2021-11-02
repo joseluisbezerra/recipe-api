70d04536434f4541e87fe70a5034e53d4c694851
joseluisbezerra <luisbezerra.work@gmail.com>
2021-11-02 20:49:04 -0300

    Adição do endpoint para criar usuário

--- code/recipe/settings.py
+++ code/recipe/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
 
     'recipe.apps.core',
+    'recipe.apps.user'
 ]
 
 MIDDLEWARE = [
