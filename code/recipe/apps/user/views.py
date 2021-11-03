ca4d0aee5ab8e950742d596e7fb550c2a0bc3410
joseluisbezerra <luisbezerra.work@gmail.com>
2021-11-03 08:49:14 -0300

    Adição do endpoint de gerenciamento de usuário

--- code/recipe/apps/user/views.py
+++ code/recipe/apps/user/views.py
@@ -3,14 +3,28 @@ from recipe.apps.user.serializers import (
     AuthTokenSerializer
 )
 
-from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework import (
+    generics,
+    authentication,
+    permissions
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
 
 
 class CreateTokenView(ObtainAuthToken):
