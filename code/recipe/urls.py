b3b1e183ab98265d5122aded297ac8ca76552e9f
joseluisbezerra <luisbezerra.work@gmail.com>
2021-11-05 15:16:27 -0300

    Adição de imagem ao modelo de receitas

--- code/recipe/urls.py
+++ code/recipe/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import (
     path,
     include
@@ -8,4 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipe.apps.recipe.urls')),
     path('api/user/', include('recipe.apps.user.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
